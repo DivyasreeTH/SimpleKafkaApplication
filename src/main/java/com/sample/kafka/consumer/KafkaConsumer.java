8321891dc6850b27019ad57ab78c8cdbb69ecdfd
Gopinath Venkataraman <nathgopi45@gmail.com>
2018-07-27 17:34:28 +0530

    Added source files

---
+++ src/main/java/com/sample/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.sample.kafka.consumer;
+
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KafkaConsumer {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    @KafkaListener(topics = "${kafka.topic.helloworld}")
+    public void receive(String payload) {
+        LOGGER.info("received payload='{}'", payload);
+        latch.countDown();
+    }
+}
